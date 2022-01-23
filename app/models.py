ff5d4b52441e9320bfa33c9216576e72bfcc710e
prince <nguhprince1@gmail.com>
2022-01-23 15:46:42 +0100

    Writing some unit tests

--- app/models.py
+++ app/models.py
@@ -1,24 +1,62 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 
 class Player(models.Model):
+    GENDER_CHOICES = (
+        ("m", _("Male")),
+        ("f", _("Female")),
+        ("nb", _("Non binary")),
+        ("rns", _("Rather not say"))
+    )
     first_name = models.CharField(max_length=30, null=False)
     last_name = models.CharField(max_length=30, null=False)
     phone = models.CharField(max_length=20, unique=True)
     level = models.IntegerField()
     classroom = models.CharField(max_length=5)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    gender = models.CharField(max_length=3, choices=GENDER_CHOICES)
 
 class Fixture(models.Model):
     level = models.IntegerField()
 
 class PlayerFixture(models.Model):
+    COLOR_CHOICES = (
+        ("White", _("White")),
+        ("Black", _("Black"))
+    )
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     fixture = models.ForeignKey(Fixture, on_delete=models.CASCADE)
+    color = models.CharField(max_length=6, choices=())
+
+    def clean(self) -> None:
+        # a fixture cannot have more than 2 player fixture entries
+        if self.fixture.playerfixture_set.all().count() > 2:
+            raise ValidationError( _("Fixture already has two players") )
+        return super().clean()
+
+    class Meta:
+        unique_together = [ ["player", "fixture"] ]
 
 class Game(models.Model):
+    PERIOD_CHOICES = (
+        ("break", _("Break")),
+        ("after_school", _("After school"))
+    )
     fixture = models.ForeignKey(Fixture, on_delete=models.CASCADE)
     date = models.DateField()
     classroom = models.CharField(max_length=5)
     period = models.CharField(max_length=30)
-    number = models.IntegerField()
+    number = models.IntegerField(default=1)
+    white_score = models.FloatField()
+
+    def clean(self) -> None:
+        # white score must either be 0, 0.5 or 1
+        if self.white_score != 0 and self.white_score != 0.5 and self.white_score != 1:
+            raise ValidationError( _("White score must either be 0, 0.5 or 1") )
+        flag = False
+        # checking if the period is in the period_choices
+        for period in self.PERIOD_CHOICES:
+            if self.period == period[0]:
+                flag = True
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,64 @@
+from . import models
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+import random
+
+first_names = ["John", "Chris", "Michael", "Peter",
+               "Andrew", "Kizito", "Randy", "Jake", "Daniel"]
+last_names = ["Radcliffe", "Specter", "Stone", "Hightower",
+              "Lisbon", "Peterson", "Anderson", "Powers"]
+phone_numbers = ["6535040440703", "6749940201814", "6747920592203",
+                 "65452io2ou48058", "68084924533203", "678342258599286"]
+genders = ["m", "f", "nb", "rns"]
+
+
+def create_random_player() -> models.Player:
+    gender = random.choice(genders)
+    first_name = random.choice(first_names)
+    last_name = random.choice(last_names)
+    
+    # get a unique phone number
+    while True:
+        phone_number = random.choice(phone_numbers)
+        if models.Player.objects.filter(phone=phone_number).count() < 1:
+            break
+
+    classroom = "se3"
+    level = 3
+    
+    return models.Player.objects.create(first_name=first_name, last_name=last_name, phone=phone_number, classroom=classroom, level=level, gender=gender)
+
+def create_fixture() -> models.Fixture:
+    level = random.choice([1, 2, 3, 4, 5])
+
+    return models.Fixture.objects.create(level=level)
+
+class PlayerFixtureTest(TestCase):
+    def test_fixture_with_more_than_two_playerfixture_entries(self):
+        """
+        fixture_with_more_than_two_playerfixture_entries should raise a ValidationError if the fixture has more than 2 playerfixture entries
+        """
+        player1 = create_random_player()
+        player2 = create_random_player()
+        player3 = create_random_player()
+        fixture = create_fixture()
+
+        for player in [player1, player2, player3]:
+            playerfixture = models.PlayerFixture.objects.create(player=player, fixture=fixture)
+
+        self.assertRaises(ValidationError, playerfixture.clean)
+    
+    def test_fixture_with_two_playerfixture_entries(self):
+        """
+        fixture_with_two_playerfixture_entries shouldn't raisea ValidationError if the fixture has excatly 2 playerfixture entries
+        """
+        player1 = create_random_player()
+        player2 = create_random_player()
+        fixture = create_fixture()
+
+        for player in [player1, player2]:
+            playerfixture = models.PlayerFixture.objects.create(player=player, fixture=fixture)
+
+        self.assertIsNone(playerfixture.clean)
