00197694616955461501e5a89f0a1db6c0233171
prince <nguhprince1@gmail.com>
2022-03-21 17:17:21 +0100

    Added appShellFiles constant to the serviceworker.js file

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,5 +1,22 @@
 var staticCacheName = 'djangopwa-v1';
 
+const appShellFiles = [
+	"/static/app/css/index.css",
+	"/static/app/css/styles.css",
+	"/static/app/js/index.js",
+	"/static/app/js/jquery-3.6.0.min.js",
+	"/static/app/js/functions.js",
+	"/static/app/js/home.js",
+	"/static/app/js/tournaments.js",
+	"/static/app/bootstrap/css/booststrap.min.css",
+	"/static/app/css/booststrap-dark.min.css",
+	"/static/app/bootstrap/js/bootstrap.min.js",
+	"/static/app/bootstrap/js/bootstrap.bundle.min.js",
+	"/static/app/font-awesome/css/all.css",
+	"/static/app/js/luxon.min.js",
+	"/jsi18n/"
+]
+
 self.addEventListener('install', function(event) {
 event.waitUntil(
 	caches.open(staticCacheName).then(function(cache) {
